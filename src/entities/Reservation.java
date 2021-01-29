4f1fedd3a048655c181874819db0b03930674b69
rodrigodojo <rodrigodoj@gmail.com>
2021-01-29 11:38:33 -0300

    Atualizando Reservation

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -1,10 +1,58 @@
 package entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
 	
 	private Integer roomNumber ;
-	private Date checkin , checkout;
+	private Date checkIn , checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation() {
+		
+	}
+		
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
 
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MICROSECONDS);
+	}
+	
+	public void updateDates(Date checkIn , Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room " + roomNumber + ", Check in: " + sdf.format(checkIn) + ", Check-out: " + sdf.format(checkOut) +"," + duration()+ "night";
+	}
 }
